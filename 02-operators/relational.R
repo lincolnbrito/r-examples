d8e44d8c521dfaf3af4337a1f2cb4c1a464f056d
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-04-04 12:52:33 -0300

    relational

---
+++ 02-operators/relational.R
@@ -0,0 +1,27 @@
+" 
+  Relational operators are used to compare 
+  between values. Here is a list of relational operators available in R.
+
+  < less than
+  > greater than
+  <= less than or equal to
+  >= greter than or equal to
+  == equal to
+  != not equal to
+
+"
+
+x <- 5
+y <- 16
+
+x < y
+
+x > y
+
+x <= 5
+
+y >= 20
+
+y == 16
+
+x != 5
