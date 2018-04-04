a0227bb8f2541d2cd9f3eefa1eba277168072a24
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-04-04 17:27:22 -0300

    environment

--- 03-functions/environment.R
+++ 03-functions/environment.R
@@ -5,3 +5,19 @@ f <- function(x) x<-0
 
 f(1)
 ls()
+
+
+f <- function(f_x) {
+  g <- function(g_x) {
+    print("Inside g")
+    print(environment())
+    print(ls())
+  }
+  
+  g(5)
+  print("Inside f")
+  print(environment())
+  print(ls())
+}
+
+f(6)
