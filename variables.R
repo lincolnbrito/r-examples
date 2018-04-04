ae768b8cd85fd12c63c7e5acb1f67661b3652937
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-04-04 12:25:47 -0300

    variables and constants

---
+++ variables.R
@@ -0,0 +1,7 @@
+# Variables are used to store 
+# data, whose value can be changed 
+# according to our need. Unique name given 
+# to variable (function and objects as well) is identifier.
+
+# Valid identifiers in R
+# total, Sum, .fine.with.dot, this_is_acceptable, Number5
