b949d5c85be07ff070ac7db1dea191827bcd8f89
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-04-04 13:57:04 -0300

    assignment operators

---
+++ 02-operators/assignment.R
@@ -0,0 +1,19 @@
+"
+  operators used to assign values to variables
+  
+  <-, <<-, =  leftwards assignment
+  ->, ->>     rightwards assignment
+  
+  The operators <- and = can be used, almost interchangeably, to assign to variable in the same environment.
+  The <<- operator is used for assigning to variables in the parent environments (more like global assignments). The rightward assignments, although available are rarely used.
+  
+"
+
+x <- 5
+x
+
+x = 9
+x
+
+10 -> x
+x
