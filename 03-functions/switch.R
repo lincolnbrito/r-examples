32de771b6a8a7232ad9bfdd518460574a48054cb
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-04-04 18:25:57 -0300

    switch

---
+++ 03-functions/switch.R
@@ -0,0 +1,13 @@
+" switch(expression, list) "
+
+switch(2,"red","green","yellow")
+switch(1,"red","green","yellow")
+
+x <- switch(4,"red","green","yellow")
+x
+
+x <- switch(0,"red","green","yellow")
+x
+
+switch("color", "color"="red", "shape"="square","length"=5)
+switch("length", "color"="red", "shape"="square","length"=5)
