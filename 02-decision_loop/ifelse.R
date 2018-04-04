4622f590e0b01051edb5583db418485e79951541
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-04-04 14:05:47 -0300

    if else statement

---
+++ 02-decision_loop/ifelse.R
@@ -0,0 +1,14 @@
+x <- 5
+
+if ( x > 0 ) {
+  print("Non-negative number")
+} else {
+  print("Negative number")
+}
+
+# conditional can be written in a single line
+if (x > 0) print("Non-negative number") else print("Negative number")
+
+x <- -5 
+y <- if(x > 0) 5 else 6
+y
