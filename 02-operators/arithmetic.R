a6607ba403a056606856553a5fcc76096f9db8bf
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-04-04 12:43:37 -0300

    operators

---
+++ 02-operators/arithmetic.R
@@ -0,0 +1,24 @@
+" 
+  + addition
+  - subtraction
+  * multiplication
+  / division
+  ^ exponent
+  %% modulus (remainder from division)
+  %/% integer division
+" 
+
+x <- 5
+y <- 16
+
+x+y
+
+x-y
+
+x*y
+
+y%/%x
+
+y%%x
+
+y^x
